fd7bbb7d4c79cfa8d97ac1a3e837ef6322a88a83
Wada Yusuke <yusuketenpa12345@gmail.com>
2026-03-21 00:13:05 +0900

    infra fix

---
+++ infra/environments/prod/moved.tf
@@ -0,0 +1,49 @@
+moved {
+  from = module.resume_stack.google_service_account.app
+  to   = module.service_account.google_service_account.app
+}
+
+moved {
+  from = module.resume_stack.google_artifact_registry_repository.app
+  to   = module.artifact_registry.google_artifact_registry_repository.app
+}
+
+moved {
+  from = module.resume_stack.google_storage_bucket.db_backup
+  to   = module.storage.google_storage_bucket.db_backup
+}
+
+moved {
+  from = module.resume_stack.google_storage_bucket_iam_member.app_db_backup
+  to   = module.storage.google_storage_bucket_iam_member.app_db_backup
+}
+
+moved {
+  from = module.resume_stack.google_storage_bucket.frontend
+  to   = module.storage.google_storage_bucket.frontend
+}
+
+moved {
+  from = module.resume_stack.google_storage_bucket_iam_member.frontend_public
+  to   = module.storage.google_storage_bucket_iam_member.frontend_public
+}
+
+moved {
+  from = module.resume_stack.google_secret_manager_secret.app
+  to   = module.cloud_run.google_secret_manager_secret.app
+}
+
+moved {
+  from = module.resume_stack.google_secret_manager_secret_iam_member.app
+  to   = module.cloud_run.google_secret_manager_secret_iam_member.app
+}
+
+moved {
+  from = module.resume_stack.google_cloud_run_v2_service.app
+  to   = module.cloud_run.google_cloud_run_v2_service.app
+}
+
+moved {
+  from = module.resume_stack.google_cloud_run_v2_service_iam_member.public_access
+  to   = module.cloud_run.google_cloud_run_v2_service_iam_member.public_access
+}
--- infra/environments/stg/variables.tf
+++ infra/environments/stg/variables.tf
@@ -18,3 +18,9 @@ variable "cors_origins" {
   type        = string
   default     = "https://storage.googleapis.com"
 }
+
+variable "container_image_tag" {
+  description = "Container image tag used when Terraform creates Cloud Run."
+  type        = string
+  default     = "latest"
+}
