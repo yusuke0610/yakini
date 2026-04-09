9dec02399d45b54271f73a8e9a4b69bf36e7d9a6
Wada Yusuke <yusuketenpa12345@gmail.com>
2026-04-09 21:28:06 +0900

    frontend test add

--- frontend/src/test-setup.ts
+++ frontend/src/test-setup.ts
@@ -1,1 +1,14 @@
 import "@testing-library/jest-dom";
+import { afterAll, afterEach, beforeAll } from "vitest";
+import { cleanup } from "@testing-library/react";
+import { server } from "./test/mswServer";
+
+/** 各テスト後に DOM をクリーンアップ */
+afterEach(() => {
+  cleanup();
+});
+
+/** MSW サーバーのライフサイクル */
+beforeAll(() => server.listen({ onUnhandledRequest: "warn" }));
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
